c33602efb8b987e6504e8e9f488eb02cec5acb75
엄채연 EOM CHAE YEON <sorimchuku@gmail.com>
2025-07-24 09:37:49 +0900

    메인(달력)화면에서 선택한 월 반영하여 화면출력 변경, 거래 리스트 및 합계 불러오기 기능 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kkugit/data/service/category_service.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:kkugit/data/service/preference_service.dart';
